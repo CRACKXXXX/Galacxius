f0ef0999dbc58949eb7931964c72f96708bf0e0f
CRACKXXXX <joelrjtirma@gmail.com>
2026-05-13 15:40:29 +0100

    First commit "base"

---
+++ src/client/java/dev/crackorb/fabric/client/GalacxiusDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.crackorb.fabric.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GalacxiusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
